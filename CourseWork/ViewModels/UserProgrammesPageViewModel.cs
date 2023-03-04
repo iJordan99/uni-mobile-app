929f1eff72abeb33b5551e6bc2f518137d6b9a27
iJordan99 <34099612+iJordan99@users.noreply.github.com>
2023-03-04 13:05:53 +0000

    Create Programme View
    Template for the create programme view

---
+++ CourseWork/ViewModels/UserProgrammesPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CourseWork.ViewModels
+{
+	public class UserProgrammesPageViewModel
+	{
+		public UserProgrammesPageViewModel()
+		{
+		}
+	}
+}
+
--- CourseWork/Views/CreateProgrammePage.xaml.cs
+++ CourseWork/Views/CreateProgrammePage.xaml.cs
@@ -1,9 +1,18 @@
-﻿namespace CourseWork.Views;
+﻿using CourseWork.ViewModels;
+namespace CourseWork.Views;
 
 public partial class CreateProgrammePage : ContentPage
 {
-	public CreateProgrammePage()
+	public CreateProgrammePage(CreateProgrammePageViewModel vm)
 	{
 		InitializeComponent();
+		BindingContext = vm;
 	}
+
+    void EntryName_TextChanged(System.Object sender, Microsoft.Maui.Controls.TextChangedEventArgs e)
+    {
+        var viewModel = (BindingContext as CreateProgrammePageViewModel);
+        viewModel.ProgrammeName = e.NewTextValue ?? string.Empty;
+    }
 }
+
